c37c1ac84ff69e2cf987f46204233c6f2f0594ea
Pavan Baloju <pavan.baloju@thoughtworks.com>
2021-04-25 13:42:39 +0530

    [Pavan] Add. test to check savetofile and loadfromfile

--- CARDS/deck_test.go
+++ CARDS/deck_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestNewDeck(t *testing.T){
 	d := newDeck()
@@ -16,4 +19,18 @@ func TestNewDeck(t *testing.T){
 	if d[len(d) - 1] != "Four of Clubs" {
 		t.Errorf("Expected Four of Clubs but got %v", d[15])
 	}
+}
+
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T){
+	os.Remove("_decktesting")
+
+	d := newDeck()
+	d.saveToFile("_decktesting")
+
+	loadedDeck := newDeckFromFile("_decktesting")
+
+	if len(loadedDeck) != 16 {
+		t.Errorf("Expected 16 cards but got %v", len(loadedDeck))
+	}
+	os.Remove("_decktesting")
 }
