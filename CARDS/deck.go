69207272e12a48aa6e810934991ac2bd56d8c993
Pavan Baloju <pavan.baloju@thoughtworks.com>
2021-04-25 10:13:16 +0530

    [Pavan] Add newDeck() to create deck

--- CARDS/deck.go
+++ CARDS/deck.go
@@ -2,7 +2,21 @@ package main
 
 import "fmt"
 
-type deck []string
+type deck []string //a slice of strings
+
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string {"Hearts", "Spades", "Diamonds", "Clubs"}
+	cardValues := []string {"Ace","Two","Three","Four"}
+
+	for _, suit := range cardSuits{
+		for _, value := range cardValues{
+			cards  = append(cards, value +" of "+suit)
+		}
+	}
+	return cards
+}
 
 func (d deck) print(){
 	for i, card := range d{
--- CARDS/main.go
+++ CARDS/main.go
@@ -2,8 +2,7 @@ package main
 
 func main() {
 	// var card string = "Ace of Spades"
-	cards := deck{"Ace of Spades", "King of spades"}
-	cards = append(cards, "Queen of Hearts")
+	cards := newDeck()
 
 	cards.print()
 }
