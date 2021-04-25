81430869f6d815853410fbe6ba78611173fa0e9b
Pavan Baloju <pavan.baloju@thoughtworks.com>
2021-04-25 11:43:07 +0530

    [Pavan] Add deal methods that returns multiple values

--- CARDS/main.go
+++ CARDS/main.go
@@ -1,8 +1,14 @@
 package main
+import "fmt"
 
 func main() {
 	// var card string = "Ace of Spades"
 	cards := newDeck()
+	//cards.print()
 
-	cards.print()
+	hand, remainingCards := deal(cards, 5)
+	hand.print()
+	fmt.Println()
+	fmt.Println()
+	remainingCards.print()
 }
