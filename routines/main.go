c2668daaf0fd33164ed9df3a327f9f93c767fad6
Pavan Baloju <pavan.baloju@thoughtworks.com>
2021-04-26 16:49:46 +0530

    [Pavan] Add. Repeating routines

--- routines/main.go
+++ routines/main.go
@@ -17,17 +17,18 @@ func main(){
 	for _, link := range links{
 		go checkLink(link, c)
 	}
-	for i := 0; i < len(links); i++ {
-		fmt.Println(<- c)
+	for l := range c{
+		go checkLink(l, c)
+
 	}
 }
 func checkLink(link string, c chan string){
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link," might be down!")
-		c <- "Down down"
+		c <- link
 	}
 
 	fmt.Println(link, "is up!")
-	c <- "It's up"
+	c <- link
 }
