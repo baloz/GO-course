669e7fec4f237de662db2e185263a08bd4fa4d24
Pavan Baloju <pavan.baloju@thoughtworks.com>
2021-04-26 12:10:15 +0530

    [Pavan] Add. Use custom writer

--- Interfaces/HTTP/main.go
+++ Interfaces/HTTP/main.go
@@ -6,12 +6,18 @@ import (
 	"net/http"
 	"os"
 )
-
+type logWriter struct{}
 func main(){
 	resp, err := http.Get("http://google.com");
 	if err != nil {
 		fmt.Println("Error :", err)
 		os.Exit(1)
 	}
-	io.Copy(os.Stdout, resp.Body)
+	lw := logWriter{}
+	io.Copy(lw, resp.Body)
+}
+
+func (lw logWriter) Write(bs []byte) (int, error){
+	fmt.Println(string(bs))
+	return len(bs), nil
 }
